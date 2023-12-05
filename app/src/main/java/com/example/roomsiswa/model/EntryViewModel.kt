f5a7cc312920906e1dd6cd1fd2f0edab1aac3850
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-05 17:05:37 +0700

    Create variable uiStateSiswa and data class

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.roomsiswa.model
-
-import androidx.lifecycle.ViewModel
-import com.example.roomsiswa.repositori.RepositoriSiswa
-
-class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
-
-}
