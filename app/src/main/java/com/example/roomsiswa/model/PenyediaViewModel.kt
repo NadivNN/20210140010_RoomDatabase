7b88e969753b06c50f15c8075f36d58f8d86efcd
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-05 18:05:22 +0700

    fix HomeViewModel and add isi at PenyediaViewModel

--- app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/PenyediaViewModel.kt
@@ -1,6 +1,28 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.ui.theme.AplikasiSiswa
+
+object PenyediaViewModel {
+        val Factory = viewModelFactory {
+
+            initializer {
+                HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+            }
 
 
-class PenyediaViewModel{
+            initializer {
+                EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+            }
+
+        }
+        /**
+         * Fungsi ekstensi query untuk objek [Application] dan mengembalikan sebuah instance dari [AplikasiSiswa].
+         */
+        fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+            (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
 }
+
