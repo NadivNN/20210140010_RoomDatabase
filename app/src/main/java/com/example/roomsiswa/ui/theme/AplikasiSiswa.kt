39b749a1f5668485f9fd2b83c8f06f7e24a65cfc
Nadiv N <nadiv.n.ft21@mail.umy.ac.id>
2023-12-05 17:39:22 +0700

    menambahkan isi dari class AplikasiSiswa

--- app/src/main/java/com/example/roomsiswa/ui/theme/AplikasiSiswa.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/AplikasiSiswa.kt
@@ -1,8 +1,18 @@
 package com.example.roomsiswa.ui.theme
 
 import android.app.Application
+import com.example.roomsiswa.repositori.ContainerApp
+import com.example.roomsiswa.repositori.ContainerDataApp
 
 
 class AplikasiSiswa : Application(){
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan depedensi
+     */
+    lateinit var container : ContainerApp
 
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
